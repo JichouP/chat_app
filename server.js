a4872274c240556233cc66d896d44c5648dd888d
JichouP <akkunn100@gmail.com>
2018-03-10 02:26:15 +0900

    div root

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const socketio = require('socket.io');
 const io = socketio(server);
 const path = require('path');
 express.get('*', (req, res) => {
-  res.sendFile(path.resolve() + req.path);
+  res.sendFile(path.resolve('dist') + req.path);
 });
 
 io.on('connection', (socket) => {
 
 });
 
-express.listen(3000);
+express.listen(3000, () => {
+  console.log('listening to port3000');
+});
