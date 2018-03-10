23f48abfdfd1e9c9cf7223aabd010b2d915c2271
JichouP <akkunn100@gmail.com>
2018-03-10 19:14:35 +0900

    環境ができた

--- src/server.js
+++ src/server.js
@@ -3,13 +3,10 @@ const server = require('http').createServer(express);
 const socketio = require('socket.io');
 const io = socketio(server);
 const path = require('path');
-// express.get('*socket.io*', (req, res) => {
-//   res.sendFile(path.resolve('') + req.path);
-// });
 express.get('*', (req, res) => {
   res.sendFile(path.resolve('public') + req.path);
 });
-express.listen(3000, () => {
+server.listen(3000, () => {
   console.log('listening to port3000');
 });
 
