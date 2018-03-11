74e45a1a87a9937b00a26046054272fb87037003
JichouP <akkunn100@gmail.com>
2018-03-11 14:32:53 +0900

    jsテンプレート

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ const server = require('http').createServer(express);
 const socketio = require('socket.io');
 const io = socketio(server);
 const path = require('path');
+const morgan = require('morgan');
 express.get('*', (req, res) => {
-  res.sendFile(path.resolve('public') + req.path);
+  res.sendFile(`${path.resolve('public')}${req.path}`);
 });
 server.listen(3000, () => {
   console.log('listening to port3000');
