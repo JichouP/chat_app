98c588cd2c2913d779e08081c4fbf818ea3873fe
JichouP <akkunn100@gmail.com>
2018-03-15 01:25:31 +0900

    ロビーが表示された

--- src/lobby.jsx
+++ src/lobby.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import io from 'socket.io-client';
 import { SCENE_ROOM, SCENE_TITLE } from './scenes';
 
-const socket = io();
+export default class Lobby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.room = [];
+    this.state = { data: this.room }
+    this.socket = io();
+  }
 
-export default function Lobby(props) {
-  socket.emit('RoomReq')
-  let room = [];
-  let roomm = [];
-  socket.on('RoomRes', (rooms) => {
-    for(let value of rooms) {
-      room.push(<div><label htmlFor={value}>{value}</label><button id={value}>Join</button></div>)
-    }
-  });
-  return (<div>{room}</div>)
+  onRoomReq() {
+    this.socket.emit('RoomReq')
+    this.socket.on('RoomRes', (rooms) => {
+      for(let value of rooms) {
+        this.room.push(<div key={value}><label className="label label-default" htmlFor={value}>{value}</label><button id={value}>Join</button></div>)
+        this.setState({ data: this.room });
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.onRoomReq();
+  }
+
+  render() {
+    return <div>{this.room}</div>
+  }
 }
-  
