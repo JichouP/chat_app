bffb186ce1c98805270d5d8206bda2cdcb74cc31
JichouP <akkunn100@gmail.com>
2018-03-21 22:50:31 +0900

    wrap途中

--- src/server.js
+++ src/server.js
@@ -13,30 +13,6 @@ const shajs = require('sha.js');
 //log
 express.use(morgan('combined'));
 
-//mongodb
-const dbName = 'chatApp';
-const userCol = 'userData';
-const createUser = (ID, Pass) => {
-  MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-    client.db(dbName).collection(userCol).insert({'ID': ID, 'Pass': shajs('sha256').update(Pass).digest('hex')});
-    client.close();
-    resolve(client);
-  })
-}
-const readUser = (ID, Pass) => {
-  MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-    client
-      .db(dbName)
-      .collection(userCol)
-      .find({'ID': ID, 'Pass': shajs('sha256').update(Pass).digest('hex')})
-      .toArray((err, docs) => {
-        if(docs === undefined) {
-          return 0;
-        }
-        
-      })
-  })
-}
 //rooting
 express.get('*', (req, res) => {
   res.sendFile(`${path.resolve('public')}${req.path}`);
@@ -45,64 +21,116 @@ server.listen(3000, () => {
   console.log('listening to port3000');
 });
 
+//mongodb
+const dbName = 'chatApp';
+const userCol = 'userData';
+
 //storage of socketid
 let socketid = {};
 
-io.on('connection', (socket) => {
-  console.log('Connection Success!!!');
-  //Login
-  socket.on('LoginReq', (ID, Pass) => {
-    console.log(`[Login] ID:${ID}, PassWord:${Pass}`);
-    MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-      assert.equal(err, null);
-      console.log("Connect to MongoDB!");
-      client.db(dbName).collection('userData').find({'ID': ID, 'Pass': shajs('sha256').update(Pass).digest('hex')}).toArray((err, docs) => {
-        if (docs[0] === undefined) {
-          console.log('Not Found!');
-          return 0;
-        }
+//connect to Mongo
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  /**
+   * Insert a object to DB
+   * @param {string} db
+   * @param {string} collection
+   * @param {Object} obj
+   */
+  const insertData = (db, collection, obj) => {
+    client
+      .db(db)
+      .collection(collection)
+      .insert(obj);
+  };
+
+  /**
+   * Find a object from DB
+   * @param {string} db
+   * @param {string} collection
+   * @param {Object} obj
+   * @returns {Boolean} isFound?
+   */
+  const isDataExist = (db, collection, obj) => {
+    new Promise((resolve, reject) => {
+      client
+        .db(db)
+        .collection(collection)
+        .find(obj)
+        .toArray((err, docs) => {
+          if (docs[0] === undefined) {
+            resolve(false);
+          }
+          resolve(true);
+        });
+    });
+  };
+
+  /**
+   * Create a user data
+   * @param {string} ID
+   * @param {string} Pass
+   */
+  const createUser = (ID, Pass) => {
+    insertData(dbName, userCol, { ID: ID, Pass: Pass });
+  };
+
+  /**
+   * Create a hash data
+   * @param {string} data
+   * @returns {string} Hash
+   */
+  const createHash = data =>
+    shajs('sha256')
+      .update(data)
+      .digest('hex');
+
+  //socket
+  io.on('connection', socket => {
+    console.log('Connection Success!!!');
+    //Login
+    socket.on('LoginReq', (ID, Pass) => {
+      console.log(`[Login] ID:${ID}, PassWord:${Pass}`);
+      if (isDataExist(dbName, userCol, { ID: ID, Pass: createHash(Pass) })) {
         console.log(`Found UserData! ID:${docs[0].ID}, Pass:${docs[0].Pass}`);
         socketid[socket.id] = ID;
         io.to(socket.id).emit('LoginSuccess');
         console.log(socketid);
-      })
-      client.close();
+      } else {
+        console.log('Not Found!');
+      }
     });
-    
-  });
-  //Regist
-  socket.on('RegistReq', (ID, Pass) => {
-    console.log(`[Regist] ID:${ID}, PassWord:${Pass}, Hash:${shajs('sha256').update(Pass).digest('hex')}`);
-    const promise = new Promise((resolve, reject) => {
-      MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-        assert.equal(err, null);
-        console.log("Connect to MongoDB!");
-        client.db(dbName).collection('userData').find({'ID': ID}).toArray((err, docs) => {
-          if (docs[0] !== undefined) {
-            console.log('There are already same ID');
-            io.to(socket.id).emit('RegistFailed')
-          }else {
-            resolve();
-          }
-        client.close();
-        })
+    //Regist
+    socket.on('RegistReq', (ID, Pass) => {
+      console.log(
+        `[Regist] ID:${ID}, PassWord:${Pass}, Hash:${shajs('sha256')
+          .update(Pass)
+          .digest('hex')}`
+      );
+      console.log('bb' + isDataExist(dbName, userCol, { ID: ID, Pass: createHash(Pass) }));
+      const promise = new Promise((resolve, reject) => {
+        if (isDataExist(dbName, userCol, { ID: ID, Pass: createHash(Pass) })) {
+          console.log('There are already same ID');
+          io.to(socket.id).emit('RegistFailed');
+        } else {
+          resolve();
+        }
       });
-    })
-    promise.then(
-      createUser(ID, Pass)
-    ).catch(() => {
-      console.log('There are error');
+      promise.then(createUser(ID, createHash(Pass))).catch(() => {
+        console.log('There are error');
+      });
     });
-  })
-  //Request Room in Lobby
-  let rooms = ["room1", "room2", "room3"];
-  socket.on('RoomReq', () => {
-    io.to(socket.id).emit('RoomRes', rooms);
+
+    //Request Room in Lobby
+    let rooms = ['room1', 'room2', 'room3'];
+    socket.on('RoomReq', () => {
+      io.to(socket.id).emit('RoomRes', rooms);
+    });
+
+    //Enter Room Request
+    socket.on('EnterReq', value => {
+      console.log(value);
+      socket.join(value);
+      io.to(value).emit('EnterRes', value);
+    });
   });
-  //Enter Room Request
-  socket.on('EnterReq', (value) => {
-    console.log(value);
-    socket.join(value);
-    io.to(value).emit('EnterRes', value);
-  })
 });
